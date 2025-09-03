281dbd42a117e6cc77b74f74d85b04e1f7f9716a
Eduardo Oliveira <emoliveira97@gmail.com>
2025-09-02 21:41:48 -0300

    Incluido métodos de criar e atualizar de Categoria e seus respectivos Dto

--- src/main/java/com/senac/projeto2/controller/CategoriaController.java
+++ src/main/java/com/senac/projeto2/controller/CategoriaController.java
@@ -1,11 +1,14 @@
 package com.senac.projeto2.controller;
 
+import com.senac.projeto2.dto.request.CategoriaDtoRequest;
+import com.senac.projeto2.dto.response.CategoriaDtoResponse;
 import com.senac.projeto2.dto.response.UsuarioDtoResponse;
 import com.senac.projeto2.entity.Categoria;
 import com.senac.projeto2.repository.CategoriaRepository;
 import com.senac.projeto2.service.CategoriaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -36,6 +39,21 @@ public class CategoriaController {
         }else{
             return ResponseEntity.ok(categoria);
         }
+    }
+
+    @PostMapping("/criar")
+    @Operation(summary = "Cria uma nova categoria no sistema.")
+    public ResponseEntity<CategoriaDtoResponse> criar(@Valid @RequestBody CategoriaDtoRequest categoriaDtoRequest){
+
+        return  ResponseEntity.ok(categoriaService.salvar(categoriaDtoRequest));
+    }
+
+    @PutMapping("/atualizar/{idCategoria}")
+    @Operation(summary = "Atualiza todos os parametros de uma categoria.")
+    public ResponseEntity<CategoriaDtoResponse>atualizar(
+            @Valid @PathVariable("idCategoria") Integer idCategoria,
+            @RequestBody CategoriaDtoRequest categoriaDtoRequest){
+        return ResponseEntity.ok(categoriaService.atualizar(idCategoria, categoriaDtoRequest));
     }
 
     @DeleteMapping("/apagar/{idCategoria}")
--- src/main/java/com/senac/projeto2/service/CategoriaService.java
+++ src/main/java/com/senac/projeto2/service/CategoriaService.java
@@ -1,14 +1,24 @@
 package com.senac.projeto2.service;
 
+import com.senac.projeto2.dto.request.CategoriaDtoRequest;
+import com.senac.projeto2.dto.response.CategoriaDtoResponse;
 import com.senac.projeto2.entity.Categoria;
 import com.senac.projeto2.entity.Usuario;
 import com.senac.projeto2.repository.CategoriaRepository;
+import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
 
 import java.util.List;
 
 @Service
 public class CategoriaService {
+    
+    @Autowired
+    private ModelMapper modelMapper;
+    
     private final CategoriaRepository categoriaRepository;
 
     public CategoriaService(CategoriaRepository categoriaRepository) {
@@ -22,7 +32,29 @@ public class CategoriaService {
     public Categoria listarCategoriaPorId(int idCategoria){
         return this.categoriaRepository.obterCategoriaAtivaPorId(idCategoria);
     }
+    
+    public CategoriaDtoResponse salvar(CategoriaDtoRequest categoriaDtoRequest){
+        Categoria categoria = modelMapper.map(categoriaDtoRequest, Categoria.class);
+        categoria.setStatus(1);
+        
+        Categoria categoriaSalva = this.categoriaRepository.save(categoria);
+        
+        return modelMapper.map(categoriaSalva, CategoriaDtoResponse.class);
+    }
 
+    public CategoriaDtoResponse atualizar(Integer idCategoria, CategoriaDtoRequest categoriaDtoRequest) {
+        Categoria categoria = this.listarCategoriaPorId(idCategoria);
+        if (categoria != null){
+            modelMapper.map(categoriaDtoRequest, categoria);
+            Categoria categoriaTemp = this.categoriaRepository.save(categoria);
+
+            return modelMapper.map(categoriaTemp, CategoriaDtoResponse.class);
+        }else{
+            return null;
+        }
+    }
+    
+    
     public void apagar(Integer idCategoria){
         Categoria categoria = listarCategoriaPorId(idCategoria);
         if (categoria != null) {
@@ -31,5 +63,6 @@ public class CategoriaService {
             System.out.println("Id não existe no banco de dados.");
         }
     }
+
 
 }
